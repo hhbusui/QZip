b97c9afeb1c94ab3413bd50a730f7ad7873dd7dc
h1530687053 <1530687053@qq.com>
2019-03-12 10:08:24 +0800

    Qt quazip demo

---
+++ mainwindow.cpp
@@ -0,0 +1,41 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+
+
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_btnZip_clicked()
+{
+    // 压缩文件
+    JlCompress::compressDir(ui->txtYSName->text() ,ui->txtYSDir->text());
+
+
+//    JlCompress::compressDir("F:\\a.zip", "F:\\a");
+
+
+
+}
+
+void MainWindow::on_btnUpZip_clicked()
+{
+    // 解压文件
+    JlCompress::extractDir(ui->txtJYName->text(), ui->txtJYPath->text());
+
+}
---
+++ mainwindow.h
@@ -0,0 +1,32 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+
+#include "JlCompress.h"
+
+
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_btnZip_clicked();
+
+    void on_btnUpZip_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
